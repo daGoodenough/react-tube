42790a3eb3b4bf867ecd7cbf5527192ec6765d32
unknown <dgoodenough@proton.me>
2022-11-10 17:17:05 -0800

    Whenever the search bar value changes, make a request to the youtube API, and set the state of the APP to contain that response

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SearchBar from "./components/SearchBar";
+import "./index.css";
+import axios from "axios";
+const API_KEY = process.env.REACT_APP_YOUTUBE_API;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.addSearchResults = this.addSearchResults.bind(this);
+    this.videoSearch = this.videoSearch.bind(this);
+  }
+
+  addSearchResults(res) {
+    const videoData = res.data.items
+    const videos = videoData.map((video) => {
+      return {
+        title: video.snippet.title,
+        description: video.snippet.description,
+        thumbnailMed: video.snippet.thumbnails.medium
+      }
+    })
+    this.setState({videos: videos, selectedVideo: videos[0]});
+  }
+
+  videoSearch(term) {
+    const url = "https://www.googleapis.com/youtube/v3/search";
+
+    const params = {
+      part: "snippet",
+      key: API_KEY,
+      q: term,
+      type: "video",
+    };
+
+    axios.get('./data.json')
+    // axios
+    //   .get(url, { params: params })
+      .then((response) => {
+        this.addSearchResults(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={this.videoSearch} />
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
